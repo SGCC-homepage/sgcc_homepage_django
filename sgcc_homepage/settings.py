08cfaa9049d619ff68fdbbdaabad5b35016f2ec9
myeee <mye785c83@gmail.com>
2021-03-21 12:49:29 +0900

    change STATIC_ROOT

--- sgcc_homepage/settings.py
+++ sgcc_homepage/settings.py
@@ -124,7 +124,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
 
 
 
