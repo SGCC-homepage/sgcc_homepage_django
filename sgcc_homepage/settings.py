16a013909c8c03010608b292e3e431fbced5f926
myeee <mye785c83@gmail.com>
2021-03-29 00:13:36 +0900

    고정 ip 변경

--- sgcc_homepage/settings.py
+++ sgcc_homepage/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r-3o!(k9bb#h04@slh4tk7(ox44+v1-$#=#zud(4*)fz_)#km)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['3.19.95.97', '127.0.0.1', 'sgcc.me', 'www.sgcc.me']
+ALLOWED_HOSTS = ['18.190.43.125', '127.0.0.1', 'sgcc.me', 'www.sgcc.me']
 
 AUTH_USER_MODEL = 'user.User'
 
