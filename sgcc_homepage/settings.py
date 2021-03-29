a2b7ec3f0c85f264b0dd502bd78e7916ce2f9ceb
myeee <mye785c83@gmail.com>
2021-03-30 04:12:07 +0900

    관리자 페이지 버튼 수정

--- sgcc_homepage/settings.py
+++ sgcc_homepage/settings.py
@@ -23,7 +23,7 @@ ROOT_DIR = os.path.dirname(BASE_DIR)
 SECRET_KEY = 'r-3o!(k9bb#h04@slh4tk7(ox44+v1-$#=#zud(4*)fz_)#km)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['18.190.43.125', '127.0.0.1', 'sgcc.me', 'www.sgcc.me']
 
